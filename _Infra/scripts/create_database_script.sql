60e7462769f34bdf4f71177d8d9bcf63b2654824
JartiX <artempilavin@gmail.com>
2025-02-07 18:24:20 +0800

    Доработка
    - Создан pgSQL скрипт
    - Добавлена таблица и данные поправок по температуре
    - Созданы связи по ключам
    - Создан тип данных interpolation
    - Написан pgSQL скрипт для расчета поправки к температуре с применением интерполяции

--- _Infra/scripts/create_database_script.sql
+++ _Infra/scripts/create_database_script.sql
@@ -1,210 +1,176 @@
--- measurement batch table
-CREATE TABLE IF NOT EXISTS public.measurement_batch
-(
-    id integer NOT NULL,
-    start_period timestamp without time zone DEFAULT now(),
-    position_x numeric(8,2),
-    position_y numeric(8,2),
-    user_id integer NOT NULL,
-    CONSTRAINT measurement_batch_pkey PRIMARY KEY (id)
-)
+do $$
 
-TABLESPACE pg_default;
+BEGIN
+	-- Справочник должностей
+	CREATE TABLE IF NOT EXISTS military_ranks
+	(
+		id integer primary KEY NOT NULL,
+		description character varying(255)
+	);
 
-ALTER TABLE IF EXISTS public.measurement_batch
-    OWNER to admin;
+	INSERT INTO military_ranks(id, description)
+	VALUES
+		(1, 'Рядовой'),
+		(2, 'Лейтенант')
+	ON conflict (id) do nothing;
 
--- measurement parameters table
-CREATE TABLE IF NOT EXISTS public.measurement_params
-(
-    id integer NOT NULL,
-    measurement_type_id integer NOT NULL,
-    measurement_batch_id integer NOT NULL,
-    height numeric(8,2) DEFAULT 100,
-    temperature numeric(8,2) DEFAULT 15,
-    pressure numeric(8,2) DEFAULT 750,
-    wind_speed numeric(8,2) DEFAULT 0,
-    wind_direction numeric(8,2) DEFAULT 0,
-    bullet_speed numeric(8,2),
-    CONSTRAINT measurement_params_pkey PRIMARY KEY (id)
-)
+	CREATE SEQUENCE IF NOT EXISTS military_ranks_seq START 3;
 
-TABLESPACE pg_default;
+	ALTER TABLE military_ranks
+		ALTER COLUMN id
+		SET DEFAULT nextval('public.military_ranks_seq');
 
-ALTER TABLE IF EXISTS public.measurement_params
-    OWNER to admin;
+	-- Пользователя
+	CREATE TABLE IF NOT EXISTS employees
+	(
+		id integer primary KEY NOT NULL,
+		name text,
+		birthday timestamp ,
+		military_rank_id integer
+	);
 
--- measurement types table
-CREATE TABLE IF NOT EXISTS public.measurement_types
-(
-    id integer NOT NULL,
-    name character varying(100) COLLATE pg_catalog."default",
-    CONSTRAINT measurement_types_pkey PRIMARY KEY (id)
-)
+	INSERT INTO employees(id, name, birthday,military_rank_id )  
+	VALUES
+		(1, 'Воловиков Александр Сергеевич','1978-06-24', 2),
+		(2, 'Олег Михайлович Залыгин', '1990-10-12', '1')
+	ON conflict (id) do nothing;
 
-TABLESPACE pg_default;
+	CREATE SEQUENCE IF NOT EXISTS employees_seq START 2;
 
-ALTER TABLE IF EXISTS public.measurement_types
-    OWNER to admin;
+	ALTER TABLE employees
+		ALTER COLUMN id
+		SET DEFAULT nextval('public.employees_seq');
 
--- ranks table
-CREATE TABLE IF NOT EXISTS public.ranks
-(
-    id integer NOT NULL,
-    rank_title character varying(100) COLLATE pg_catalog."default",
-    CONSTRAINT ranks_pkey PRIMARY KEY (id)
-)
+	ALTER TABLE public.employees
+		DROP CONSTRAINT IF EXISTS military_rank_id_constraint,
+		ADD CONSTRAINT military_rank_id_constraint
+		FOREIGN KEY (military_rank_id)
+		REFERENCES public.military_ranks(id);
 
-TABLESPACE pg_default;
+	-- Устройства для измерения
+	CREATE TABLE IF NOT EXISTS measurement_types
+	(
+	id integer primary KEY NOT NULL,
+	short_name  character varying(50),
+	description text 
+	);
 
-ALTER TABLE IF EXISTS public.ranks
-    OWNER to admin;
+	INSERT INTO measurement_types(id, short_name, description)
+	VALUES
+		(1, 'ДМК', 'Десантный метео комплекс'),
+		(2,'ВР','Ветровое ружье')
+	ON conflict (id) do nothing;
 
--- users table
-CREATE TABLE IF NOT EXISTS public.users
-(
-    id integer NOT NULL,
-    username character varying(100) COLLATE pg_catalog."default",
-    rank_id integer NOT NULL,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
-)
+	CREATE SEQUENCE IF NOT EXISTS measurement_types_seq START 3;
 
-TABLESPACE pg_default;
+	ALTER TABLE measurement_types
+		ALTER COLUMN id
+		SET DEFAULT nextval('public.measurement_types_seq');
 
-ALTER TABLE IF EXISTS public.users
-    OWNER to admin;
+	-- Таблица с параметрами
+	CREATE TABLE IF NOT EXISTS measurement_input_params
+	(
+		id integer primary KEY NOT NULL,
+		measurement_type_id integer NOT NULL,
+		height numeric(8,2) DEFAULT 0,
+		temperature numeric(8,2) DEFAULT 0,
+		pressure numeric(8,2) DEFAULT 0,
+		wind_direction numeric(8,2) DEFAULT 0,
+		wind_speed numeric(8,2) DEFAULT 0
+	);
 
--- sequence for users id
-CREATE SEQUENCE IF NOT EXISTS public.users_sequence
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
+	INSERT INTO measurement_input_params(id, measurement_type_id, height, temperature, pressure, wind_direction,wind_speed )
+	VALUES
+		(1, 1, 100,12,34,0.2,45)
+	ON conflict (id) do nothing;
 
-ALTER SEQUENCE public.users_sequence
-    OWNED BY public.users.id;
+	CREATE SEQUENCE IF NOT EXISTS measurement_input_params_seq START 2;
 
-ALTER SEQUENCE public.users_sequence
-    OWNER TO admin;
+	ALTER TABLE measurement_input_params
+		ALTER COLUMN id
+		SET DEFAULT nextval('public.measurement_input_params_seq');
 
--- sequence for ranks id
-CREATE SEQUENCE IF NOT EXISTS public.ranks_sequence
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
+	ALTER TABLE public.measurement_input_params
+		DROP CONSTRAINT IF EXISTS measurement_type_id_constraint,
+		ADD CONSTRAINT measurement_type_id_constraint
+		FOREIGN KEY (measurement_type_id)
+		REFERENCES public.measurement_types(id);
 
-ALTER SEQUENCE public.ranks_sequence
-    OWNED BY public.ranks.id;
+	-- Таблица с историей
+	CREATE TABLE IF NOT EXISTS measurement_batches
+	(
+			id integer primary KEY NOT NULL,
+			employee_id integer NOT NULL,
+			measurement_input_param_id integer NOT NULL,
+			started timestamp DEFAULT now()
+	);
 
-ALTER SEQUENCE public.ranks_sequence
-    OWNER TO admin;
 
--- sequence for measurement parameters id
-CREATE SEQUENCE IF NOT EXISTS public.measurement_params_sequence
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
+	INSERT INTO measurement_batches(id, employee_id, measurement_input_param_id)
+	VALUES
+		(1, 1, 1)
+	ON conflict (id) do nothing;
 
-ALTER SEQUENCE public.measurement_params_sequence
-    OWNED BY public.measurement_params.id;
+	CREATE SEQUENCE IF NOT EXISTS measurement_batches_seq START 2;
 
-ALTER SEQUENCE public.measurement_params_sequence
-    OWNER TO admin;
+	ALTER TABLE measurement_batches
+		ALTER COLUMN id
+		SET DEFAULT nextval('public.measurement_batches_seq');
 
--- sequence for measurement batch id
-CREATE SEQUENCE IF NOT EXISTS public.measurement_batch_sequence
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 9223372036854775807
-    CACHE 1;
+	ALTER TABLE public.measurement_batches
+		DROP CONSTRAINT IF EXISTS employee_id_constraint,
+		ADD CONSTRAINT employee_id_constraint
+		FOREIGN KEY (employee_id)
+		REFERENCES public.employees(id);
 
-ALTER SEQUENCE public.measurement_batch_sequence
-    OWNED BY public.measurement_batch.id;
+	ALTER TABLE public.measurement_batches
+		DROP CONSTRAINT IF EXISTS measurement_input_param_id_constraint,
+		ADD CONSTRAINT measurement_input_param_id_constraint
+		FOREIGN KEY (measurement_input_param_id)
+		REFERENCES public.measurement_input_params(id);
 
-ALTER SEQUENCE public.measurement_batch_sequence
-    OWNER TO admin;
+	-- Столбец с датой в measurement_types
+	ALTER TABLE public.measurement_types
+	ADD COLUMN IF NOT EXISTS started timestamp DEFAULT now();
 
--- using increment for id fields
-alter table public.measurement_batch alter column
-id set default nextval('public.measurement_batch_sequence');
+	-- Таблица корректировки температуры
+	CREATE TABLE IF NOT EXISTS temperature_correction
+	(
+		id integer primary KEY NOT NULL,
+		temperature numeric(8,2),
+		delta_t numeric(8,2)
+	);
 
-alter table public.measurement_params alter column
-id set default nextval('public.measurement_params_sequence');
+	INSERT INTO public.temperature_correction(id, temperature, delta_t)
+	VALUES
+		(1, 0, 0),
+		(2, 5, 0.5),
+		(3, 10, 1),
+		(4, 15, 1),
+		(5, 20, 1.5),
+		(6, 25, 2),
+		(7, 30, 3.5),
+		(8, 40, 4.5)
+	ON conflict (id) do nothing;
+		
+	CREATE SEQUENCE IF NOT EXISTS temperature_correction_seq START 9;
 
-alter table public.users alter column
-id set default nextval('public.users_sequence');
+	ALTER TABLE temperature_correction
+		ALTER COLUMN id
+		SET DEFAULT nextval('public.temperature_correction_seq');
 
-alter table public.ranks alter column
-id set default nextval('public.ranks_sequence');
+	-- Тип данных для хранения точек интерполяции
+	IF NOT EXISTS (
+		SELECT 1
+		FROM pg_type
+		WHERE typname = 'interpolation'
+	) THEN
+		CREATE TYPE interpolation AS (
+			temp_low float,
+			delta_low float,
+			temp_high float,
+			delta_high float
+		);
+	END IF;
 
--- fill with rows
-insert into public.ranks (rank_title)
-values
-    ('Рядовой'),
-    ('Ефрейтор'),
-    ('Младший сержант'),
-    ('Сержант'),
-    ('Старший сержант'),
-    ('Старшина'),
-    ('Прапорщик'),
-    ('Старший прапорщик'),
-    ('Младший лейтенант'),
-    ('Лейтенант'),
-    ('Старший лейтенант'),
-    ('Капитан'),
-    ('Майор'),
-    ('Подполковник'),
-    ('Полковник'),
-    ('Генерал-майор'),
-    ('Генерал-лейтенант'),
-    ('Генерал-полковник'),
-    ('Генерал армии'),
-    ('Маршал Российской Федерации'),
-    ('Матрос'),
-    ('Старший матрос'),
-    ('Старшина 2 статьи'),
-    ('Старшина 1 статьи'),
-    ('Главный старшина'),
-    ('Главный корабельный старшина'),
-    ('Мичман'),
-    ('Старший мичман'),
-    ('Капитан-лейтенант'),
-    ('Капитан 3 ранга'),
-    ('Капитан 2 ранга'),
-    ('Капитан 1 ранга'),
-    ('Контр-адмирал'),
-    ('Вице-адмирал'),
-    ('Адмирал'),
-    ('Адмирал флота');
-
-insert into public.measurement_types (id, name)
-values
-	(1, 'ДМК'),
-	(2, 'ВР');
-
-insert into public.users (username, rank_id)
-values
-	('alena', 1),
-	('nikita', 13);
-
-insert into public.measurement_batch (start_period, user_id)
-values
-	('2025-01-31 11:24', 1),
-	('2025-01-31 11:26', 2);
-
-insert into public.measurement_params (measurement_type_id, measurement_batch_id, height, temperature, pressure, wind_speed, wind_direction, bullet_speed)
-values
-	(2, 1, 100, 14, 720, 14, 25, 10),
-	(1, 2, 100, 14, 720, 14, 25, 10);
-
--- select * from public.users;
--- select * from public.ranks;
--- select * from public.measurement_batch;
--- select * from public.measurement_types;
--- select * from public.measurement_params;
+END $$;
